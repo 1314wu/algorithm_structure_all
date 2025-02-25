555e179215272f1a073eaa9a55d1af29a4dfa6d1
wzq <873193821@qq.com>
2025-02-25 14:40:01 +0800

    heap sort support min heap

--- 76/76_heap_sort.cpp
+++ 76/76_heap_sort.cpp
@@ -6,7 +6,38 @@ void swap(int *a, int *b) {
 	*b = *a;
 	*a = temp;
 }
-
+void min_heapify(int arr[], int start, int end) {
+	int dad = start;
+	int son = dad * 2 + 1;
+	for (son = dad * 2 + 1; son <= end; son = son * 2 + 1) {
+		if (son + 1 <= end && arr[son] > arr[son + 1]) {
+			son++;
+		}
+		if (arr[dad] < arr[son]) {
+			return;
+		}
+		else {
+			//子节点比父节点小
+			swap(&arr[dad], &arr[son]);
+			dad = son;
+		}
+	}
+}
+void heap_sort_min(int arr[], int len) {
+	int id;
+	for (id = len / 2 - 1; id >= 0; id--) {
+		min_heapify(arr, id, len-1);
+	}
+	for (id = len - 1; id >= 0; id--) {
+		swap(&arr[0], &arr[id]);
+		min_heapify(arr, 0, id - 1);
+	}
+	printf("Min Order: ");
+	for (id = 0; id < len; id++) {
+		printf("%d ", arr[id]);
+	}
+	printf("\n");
+}
 void max_heapify(int arr[], int start, int end) {
 
 	int dad = start;
@@ -54,11 +85,18 @@ void heap_sort(int arr[], int len) {
 int main() {
 	//int arr[] = { 3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6 };
 	int arr[] = { 49,38,65,97,76,13,27,49 };
+	//int arr[] = { 2,1 };
 	int len = (int) sizeof(arr) / sizeof(*arr);
 	heap_sort(arr, len);
 	int i;
 	for (i = 0; i < len; i++)
 		printf("%d ", arr[i]);
+	printf("\n");
+	int arr2[] = { 49,38,65,97,76,13,27,49 };
+	heap_sort_min(arr2, len);
+	
+	for (i = 0; i < len; i++)
+		printf("%d ", arr2[i]);
 	printf("\n");
 	return 0;
 }
